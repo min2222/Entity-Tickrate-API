d47c4a04cfefc0b32aa810b412aa427a48c90801
min <goyoumin2@gmail.com>
2024-04-21 19:06:08 +0900

    fixed very critical issue

--- src/main/java/com/min01/entitytimer/mixin/MixinEntity.java
+++ src/main/java/com/min01/entitytimer/mixin/MixinEntity.java
@@ -2,7 +2,9 @@ package com.min01.entitytimer.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.min01.entitytimer.EntityTimer;
 import com.min01.entitytimer.TimerUtil;
@@ -13,22 +15,22 @@ import net.minecraft.world.entity.Entity;
 @Mixin(Entity.class)
 public class MixinEntity
 {
-	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;addAdditionalSaveData(Lnet/minecraft/nbt/CompoundTag;)V"), method = "saveWithoutId")
-	public void addAdditionalSaveData(Entity instance, CompoundTag tag)
+	@Inject(at = @At(value = "HEAD"), method = "saveWithoutId")
+	private void saveWithoutId(CompoundTag tag, CallbackInfoReturnable<CompoundTag> cir)
 	{
-		if(TimerUtil.hasTimer(instance))
+		if(TimerUtil.hasTimer(Entity.class.cast(this)))
 		{
-			EntityTimer timer = TimerUtil.getTimer(instance);
+			EntityTimer timer = TimerUtil.getTimer(Entity.class.cast(this));
 			tag.putFloat(TimerUtil.TICKRATE, timer.tickrate);
 		}
 	}
 	
-	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;readAdditionalSaveData(Lnet/minecraft/nbt/CompoundTag;)V"), method = "load")
-	public void readAdditionalSaveData(Entity instance, CompoundTag tag)
+	@Inject(at = @At(value = "HEAD"), method = "load")
+	private void load(CompoundTag tag, CallbackInfo ci)
 	{
 		if(tag.contains(TimerUtil.TICKRATE))
 		{
-			TimerUtil.setTickrate(instance, tag.getFloat(TimerUtil.TICKRATE));
+			TimerUtil.setTickrate(Entity.class.cast(this), tag.getFloat(TimerUtil.TICKRATE));
 		}
 	}
 }
